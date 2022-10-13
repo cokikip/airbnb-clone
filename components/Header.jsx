54d52dd6ba2bb103c977313c21181c2f4d4ef11e
cokikip <cokicollinji@gmail.com>
2022-10-14 00:06:26 +0300

    feat: add search and menu icons

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,11 @@
+import {
+  Bars3Icon,
+  GlobeAltIcon,
+  MagnifyingGlassIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
 import Image from "next/image";
-
+// Bars3Icon, UserCircleIcon, UserIcon;
 function Header() {
   return (
     <header className="sticky top-0 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
@@ -13,11 +19,27 @@ function Header() {
         />
       </div>
       {/* Middle - Search*/}
-      <div>
-        <input type="text" />
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          className="pl-5 bg-transparent outline-none flex-grow
+          text-sm text-gray-600 placeholder:text-gray-400"
+          type="text"
+          placeholder="start your search"
+        />
+        <MagnifyingGlassIcon
+          className="hidden md:inline-flex h-8 bg-red-400
+         text-white rounded-full p-2 cursor-pointer md:mx-2"
+        />
       </div>
       {/* Right */}
-      <div></div>
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p>Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center border-2 p-2 rounded-full">
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 }

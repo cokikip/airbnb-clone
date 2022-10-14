f33270e2bde91f0b5c4453362ee2b6e7208a6ea3
cokikip <cokicollinji@gmail.com>
2022-10-14 11:08:38 +0300

    feat: add main section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
@@ -10,12 +11,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        {/* Header */}
-        <Header />
-        {/* Banner */}
-        {/* Body */}
-        {/* Footer */}
+      <Header />
+      <Banner />
+      <main className="max-w-7xl mx-auto">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
       </main>
     </div>
   );

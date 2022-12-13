f52d801e9670d31a8548150c0d91fee9b9c36529
cokikip <cokicollinji@gmail.com>
2022-12-13 16:37:35 +0300

    style: focus outline none

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -13,7 +13,7 @@ export const Banner = () => {
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect</p>
         <button
           className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full
-         font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 focus:rounded-full"
+         font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150 focus:rounded-full focus:outline-none"
         >
           I`m flexible
         </button>
